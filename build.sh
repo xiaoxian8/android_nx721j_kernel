fdce038f5c9458d29d7b252a9d07a5cdfc37c89a
糖小贤 <127175984+xiaoxian8@users.noreply.github.com>
2026-04-22 19:38:24 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+set -euo pipefail
+#克隆LLVM工具链
 git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 -b android14-release build-tools
 
+#设置环境变量
 export PATH=${PWD}/build-tools/clang-r487747c/bin:${PATH}
 
+#克隆Anykernel3
 git clone https://github.com/xiaoxian8/AnyKernel3.git --depth=1
 
+#合并配置文件
 ARCH=arm64 ./scripts/kconfig/merge_config.sh -m \
     arch/arm64/configs/vendor/pineapple_tuivm.config \
     arch/arm64/configs/vendor/pineapple_consolidate.config \
@@ -14,13 +19,14 @@ ARCH=arm64 ./scripts/kconfig/merge_config.sh -m \
 
 mv .config arch/arm64/configs/nx721j_defconfig
 
+#编译参数
 args=(-j$(nproc --all)
     O=out
 	ARCH=arm64
     LLVM=1
     DEPMOD=depmod
     DTC=usr/bin/dtc)
-
+#开始编译
 make ${args[@]} nx721j_defconfig
 
 make ${args[@]} all
