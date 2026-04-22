3c3a693bc398289b2bdf11ba1d1edf0730e1e653
糖小贤 <127175984+xiaoxian8@users.noreply.github.com>
2026-04-22 20:45:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,14 +22,12 @@ mv .config arch/arm64/configs/nx721j_defconfig
 args=(-j$(nproc --all)
     O=out
 	ARCH=arm64
-	CLANG_TRIPLE=aarch64-linux-gnu-
 	CROSS_COMPILE=aarch64-linux-gnu-
 	CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
     CC=clang
 	LD=ld.lld
 	AR=llvm-ar
 	NM=llvm-nm
-	AS=llvn-as
 	STRIP=llvm-strip
 	OBJCOPY=llvm-objcopy
 	OBJDUMP=llvm-objdump
