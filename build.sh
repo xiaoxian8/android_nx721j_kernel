2f07c04b60f81e7e8edee329d997f4687e5f56da
糖小贤 <127175984+xiaoxian8@users.noreply.github.com>
2026-04-22 19:25:29 +0800

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 -b android14-release build-tools
+
+export PATH=${PWD}/build-tools/clang-r487747c/bin:${PATH}
+
+git clone https://github.com/xiaoxian8/AnyKernel3.git --depth=1
+
+ARCH=arm64 ./scripts/kconfig/merge_config.sh -m \
+    arch/arm64/configs/vendor/pineapple_tuivm.config \
+    arch/arm64/configs/vendor/pineapple_consolidate.config \
+    arch/arm64/configs/oem/pineapple_diff_config \
+    arch/arm64/configs/oem/zlog_diff_config \
+    arch/arm64/configs/oem/boards/cerro_diff_config \
+
+mv ./config arch/arm64/configs/nx721j_defconfig
+
+args=(-j$(nproc --all)
+    O=out
+	ARCH=arm64
+    LLVM=1
+    DEPMOD=depmod
+    DTC=usr/bin/dtc)
+
+make ${args[@]} nx721j_defconfig
+
+make ${args[@]} all
+
+make ${args[@]} INSTALL_MOD_PATH=modules modules_install
+
+# cp $(find out -type f \( -name "Image" -o -name "dtbo.img" \)) ./
+
+cp out/arch/arm64/boot/Image AnyKernel3/Image
+
+cd AnyKernel3
+zip -r9v ../out/kernel.zip *
