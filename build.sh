777a710b2b869c71c369c777e73b46229d18a5bd
糖小贤 <127175984+xiaoxian8@users.noreply.github.com>
2026-04-22 20:05:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -25,11 +25,7 @@ mv .config arch/arm64/configs/nx721j_defconfig
 args=(-j$(nproc --all)
     O=out
 	ARCH=arm64
-	CLANG_TRIPLE=aarch64-linux-gnu-
-	CROSS_COMPILE=aarch64-linux-gnu-
-	CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
     LLVM=1
-	LLVM_IAS=1
     DEPMOD=depmod
     DTC=usr/bin/dtc)
 #开始编译
